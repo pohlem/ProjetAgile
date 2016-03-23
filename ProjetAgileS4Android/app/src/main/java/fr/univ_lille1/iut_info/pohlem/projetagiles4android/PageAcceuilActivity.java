94cf1686b7b8b0d8d2c0c4fa8557178403ae1a78
Pohle Maxime <pohle.maxime@gmail.com>
2016-03-23 14:13:02 +0100

    4.03

---
+++ ProjetAgileS4Android/app/src/main/java/fr/univ_lille1/iut_info/pohlem/projetagiles4android/PageAcceuilActivity.java
@@ -0,0 +1,50 @@
+package fr.univ_lille1.iut_info.pohlem.projetagiles4android;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class PageAcceuilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_acceuil);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_page_acceuil, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void onConnect(View view){
+        TextView tvNom = (TextView) findViewById(R.id.nomCompte);
+        TextView tvMdp = (TextView) findViewById(R.id.mdp);
+    }
+    public void onInscrire(View view){
+        Intent intentMain = new Intent(PageAcceuilActivity.this ,
+                menu.class);
+        PageAcceuilActivity.this.startActivity(intentMain);
+
+    }
+}
